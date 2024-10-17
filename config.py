034cdda1bee5540c6ade34b87b49a640fb48ac40
AmanXcrazy <rakhithenua7454@gmail.com>
2024-10-18 00:22:37 +0530

    Add files via upload

---
+++ config.py
@@ -0,0 +1,19 @@
+# Don't use quotes( " and ' )
+#SCRIPT BY VENOMxCRAZY
+  
+#Enter Your Bot Token here get it from @botfarher
+BOT_TOKEN=("8148962496:AAFCAzZn9wNW94GnbOS8FRRV5IYj7XQC2_I")
+
+  #Enter Your telegram username here without @
+OWNER_USERNAME=("TREXSHUBH")
+
+  #Enter your admin id here Get it from @missRose_bot by typing /info
+ADMIN_IDS=("5424529390")
+
+
+
+
+
+
+
+  
